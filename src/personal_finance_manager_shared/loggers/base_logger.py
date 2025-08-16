0a4031f643f556159b86970680c7dcc3957b7981
Chau Le Ngoc <playerzawesome@gmail.com>
2025-08-16 11:12:44 +0700

    Raise exception instead of pass

--- src/personal_finance_manager_shared/loggers/base_logger.py
+++ src/personal_finance_manager_shared/loggers/base_logger.py
@@ -8,24 +8,24 @@ logging.Formatter.converter = time.gmtime
 class BaseLogger(ABC):
     @abstractmethod
     def debug(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def info(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def warning(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def error(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def critical(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
 
     @abstractmethod
     def exception(self, msg: str, *args, **kwargs):
-        pass
+        raise NotImplementedError
